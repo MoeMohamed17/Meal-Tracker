0ad5e36fa15491f6b72ad8ad26ce6894e8382188
cgj-tang <67113323+cgj-tang@users.noreply.github.com>
2024-07-24 19:21:03 -0700

    separated API calls from other normal page routing

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     async rewrites() {
         return [
             {
-            source: '/:path*',
+            source: 'api/:path*',
             destination: 'http://localhost:65532/:path*',
             },
         ];
