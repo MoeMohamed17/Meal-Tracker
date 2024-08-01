30dcf7996e2a62a4a0507891778c57effdbf44b9
moemohamed <moemoe17@students.cs.ubc.ca>
2024-08-01 14:31:20 -0700

    Added images to recipe page and syled css

--- client/src/app/components/RecipeCard.js
+++ client/src/app/components/RecipeCard.js
@@ -22,16 +22,52 @@
 // export default Recipe;
 
 
+// // src/app/components/RecipeCard.js
+// import "../recipes/Recipes.css";
+// import Link from 'next/link';
+
+// const RecipeCard = ({ id, name, level, cuisine, time, image }) => {
+//   return (
+//     <Link href={`/recipes/${id}`} passHref>
+//       <div className="recipe-container">
+//         <div className="recipe-card">
+//           <img src={image} alt={name} className="recipe-image" />
+//           <div className="recipe-description">
+//             <p>
+//               <strong>Name:</strong> {name}
+//             </p>
+//             <p>
+//               <strong>Level:</strong> {level}
+//             </p>
+//           </div>
+
+//           <div className="recipe-info">
+//             <p>
+//               <strong>Cuisine:</strong> {cuisine}
+//             </p>
+//             <p>
+//               <strong>Time:</strong> {time}
+//             </p>
+//           </div>
+//         </div>
+//       </div>
+//     </Link>
+//   );
+// };
+
+// export default RecipeCard;
+
+
 // src/app/components/RecipeCard.js
 import "../recipes/Recipes.css";
 import Link from 'next/link';
 
-const RecipeCard = ({ id, name, level, cuisine, time, image }) => {
+const RecipeCard = ({ id, name, level, cuisine, time, imageUrl, caption }) => {
   return (
     <Link href={`/recipes/${id}`} passHref>
       <div className="recipe-container">
         <div className="recipe-card">
-          <img src={image} alt={name} className="recipe-image" />
+          <img src={imageUrl} alt={name} className="recipe-image" />
           <div className="recipe-description">
             <p>
               <strong>Name:</strong> {name}
@@ -39,6 +75,7 @@ const RecipeCard = ({ id, name, level, cuisine, time, image }) => {
             <p>
               <strong>Level:</strong> {level}
             </p>
+            {caption && <p><strong>Caption:</strong> {caption}</p>}
           </div>
 
           <div className="recipe-info">
